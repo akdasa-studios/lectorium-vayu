ea5da82aa05020bd4537a702877322cf2024421e
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2024-10-28 10:17:23 +0100

    feat: bake_database_for_app dag

---
+++ dags/bake_database_for_app.py
@@ -0,0 +1,90 @@
+from __future__ import annotations
+
+from datetime import datetime, timedelta
+from airflow.providers.docker.operators.docker import DockerOperator
+
+from airflow.decorators import dag, task
+from airflow.models import Variable
+from docker.types import Mount
+
+import lectorium as lectorium
+import services.aws as aws
+
+
+# ---------------------------------------------------------------------------- #
+#                                      DAG                                     #
+# ---------------------------------------------------------------------------- #
+
+@dag(
+    dag_display_name="📱 App: Bake Database for App",
+    description="Prepares the database for distribution with the application",
+    schedule='@daily',
+    start_date=datetime(2021, 1, 1),
+    catchup=False,
+    tags=["lectorium"],
+    dagrun_timeout=timedelta(minutes=60),
+    default_args={
+        "owner": "Advaita Krishna das",
+    },
+    render_template_as_native_obj=True,
+    max_active_runs=1,
+)
+def bake_database_for_app():
+
+    # ---------------------------------------------------------------------------- #
+    #                                    Config                                    #
+    # ---------------------------------------------------------------------------- #
+
+    app_bucket_name = (
+         Variable.get(lectorium.config.VAR_APP_BUCKET_NAME)
+    )
+
+    app_bucket_creds: lectorium.config.AppBucketAccessKey = (
+        Variable.get(
+            lectorium.config.VAR_APP_BUCKET_ACCESS_KEY,
+            deserialize_json=True
+        )
+    )
+
+    files = [
+        'library-dictionary-v0001',
+        'library-index-v0001',
+        'library-tracks-v0001'
+    ]
+
+
+    # ---------------------------------------------------------------------------- #
+    #                                     Steps                                    #
+    # ---------------------------------------------------------------------------- #
+
+    run_node_app = DockerOperator(
+        auto_remove=True,
+        mount_tmp_dir=False,
+        task_id='bake_database',
+        image='ghcr.io/akdasa-studios/lectorium-tools-bake-database',
+        command='node index.js',
+        docker_url='unix://var/run/docker.sock',
+        network_mode='host',
+        mounts=[Mount(source='/tmp/lectorium', target='/tools/artifacts', type='bind')],
+        environment={
+            'DATABASE_URI': 'https://lectorium:!DVxqv-aj_wW4kCXC*TdbYk*EHk6rDqD@app.lectorium.akdasa.studio/database/',
+        },
+    )
+
+    for file in files:
+        uploaded_file = aws.upload_file(
+            credentials=app_bucket_creds,
+            bucket_name=app_bucket_name,
+            object_key=f'artifacts/{file}.db',
+            file_path=f'/tmp/lectorium/{file}.db',
+        )
+
+        @task.bash(
+            task_id=f'remove_{file}',
+            task_display_name=f"Remove {file}.db")
+        def remove_file(file: str):
+            return f"rm /tmp/lectorium/{file}.db"
+
+        run_node_app >> uploaded_file >> remove_file(file)
+
+bake_database_for_app()
--- dags/services/aws/__init__.py
+++ dags/services/aws/__init__.py
@@ -4,4 +4,5 @@ from services.aws.tasks.sign_url import sign_url
 from services.aws.tasks.list_objects import list_objects
 from services.aws.tasks.get_file_size import get_file_size
 from services.aws.tasks.get_audio_duration import get_audio_duration
+from services.aws.tasks.upload_file import upload_file
 import services.aws.actions as actions
