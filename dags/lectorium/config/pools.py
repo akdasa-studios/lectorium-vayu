5f281e028d9d012ae38f8d4e0ac90b846c0c25e3
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2024-11-10 12:35:14 +0100

    fix: increase workers for vakshuddhi::process-audio

--- dags/lectorium/config/pools.py
+++ dags/lectorium/config/pools.py
@@ -3,6 +3,6 @@ from airflow.models import Pool
 
 Pool.create_or_update_pool(
     "vakshuddhi::process-audio",
-    slots=2,
+    slots=4,
     description="Vakshuddhi pool",
     include_deferred=False)
